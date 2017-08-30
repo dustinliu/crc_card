47f58a77b5afd95823344d5873c8b7e1002fb0b7
Min Lun Liu <liu.minlun@gmail.com>
2017-08-30 14:29:04 +0800

    use popup layer to create board

--- crc_board/web/static/board.js
+++ crc_board/web/static/board.js
@@ -2,3 +2,28 @@ $( function() {
     $( '.sortable' ).sortable();
     $( '.sortable' ).disableSelection();
 } );
+
+
+function show_create() {
+    $('.form').css({'display': 'inline-block'})
+}
+
+
+function create_board() {
+    var name = $('#board_name').val();
+    var desc = $('#board_desc').val();
+    $.ajax({
+        type: 'POST',
+        url: '/api/boards',
+        dataType: 'json',
+        data: { name: name, description: desc},
+        success: function () {
+            console.log('success')
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+        }
+    });
+    window.location.replace("{{ url_for('.index') }}");
+}
